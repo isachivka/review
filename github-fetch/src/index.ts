30045a6c227a217952e30f502468f1981c03fb55
isachivka <juicy.igor@gmail.com>
2020-03-01 13:37:15 +0300

    [@review/github-fetch] Move query into cron function, send query result to eventsApi by superagent

--- github-fetch/src/index.ts
+++ github-fetch/src/index.ts
@@ -1,19 +1,32 @@
 import 'cross-fetch/polyfill';
 import cron from 'node-cron';
+import superagent from 'superagent';
+import config from 'config';
 import client, { owner, repository } from './client';
 import { PullRequests } from './queries/types/PullRequests';
 import getPullRequests from './queries/getPullRequests';
 
-client.query<PullRequests>({
-  query: getPullRequests,
-  variables: { owner, repository },
-})
-  .then(({ data }) => {
-    if (data.repository) {
-      console.log(JSON.stringify(data.repository.pullRequests.nodes));
-    }
-  });
+const putApi = `${config.get('eventsApi')}/put`;
 
 cron.schedule('* * * * *', () => {
-  console.log('running a task every minute');
+  client.query<PullRequests>({
+    query: getPullRequests,
+    variables: { owner, repository },
+  })
+    .then(({ data }) => {
+      if (data.repository && data.repository.pullRequests.nodes) {
+        superagent
+          .put(putApi)
+          .set('Content-Type', 'application/json')
+          .send(data.repository.pullRequests.nodes)
+          .then((response) => {
+            console.log('[@review/github-fetch] Successfully send nodes:', response.text);
+          })
+          .catch((error) => {
+            console.log('[@review/github-fetch] Error send nodes:', error);
+          });
+      } else {
+        console.log('[@review/github-fetch] Something wrong, response do not contains PullRequests');
+      }
+    });
 });
