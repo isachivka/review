3804202c5c936c6a2caeb3fa02ee39f4e65d387b
isachivka <juicy.igor@gmail.com>
2020-03-02 14:04:33 +0300

    [@review/events] Add read/write pullRequests to mongodb

--- events/src/index.ts
+++ events/src/index.ts
@@ -1,28 +1,23 @@
 import express from 'express';
-import { MongoClient } from 'mongodb';
 import { PullRequests } from '@review/github-fetch/src/types/shared';
+import doMongo from './mongo';
 
 const port = 3000;
 const app = express();
 app.use(express.json());
 
-const url = 'mongodb://mongodb/review';
-
-MongoClient.connect(url, (err, client) => {
-  if (err) {
-    console.log('[@review/events] Connection to mongodb failed', err);
-    client.close();
-    return;
-  }
-
-  console.log('[@review/events] Connected successfully to server');
-  client.close();
-});
-
 app.put('/put', (request, response) => {
   const pullRequests: PullRequests = request.body;
-  console.log(pullRequests);
   response.status(200).send('Okay');
+
+  doMongo(async (db, callback) => {
+    const collection = db.collection('prs');
+    const prevPullRequests = await collection.find({}).toArray();
+    await collection.deleteMany({});
+    await collection.insertMany(pullRequests);
+    console.log({ prevPullRequests, pullRequests });
+    callback();
+  });
 });
 
 app.listen(port, () => {
