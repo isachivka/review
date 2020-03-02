9885013253b767b360250b1cbe28fd0e77762c43
isachivka <juicy.igor@gmail.com>
2020-03-02 14:04:33 +0300

    [@review/events] Add read/write pullRequests to mongodb

---
+++ events/src/mongo.ts
@@ -0,0 +1,24 @@
+import { Db, MongoClient } from 'mongodb';
+
+const dbName = 'review';
+const url = `mongodb://mongodb/${dbName}`;
+let init = true;
+
+function doMongo(callback: (db: Db, callback: () => void) => void) {
+  MongoClient.connect(url, (err, client) => {
+    if (err) {
+      console.log('[@review/events] Connection to mongodb failed', err);
+      client.close();
+      return;
+    }
+
+    if (init === true) {
+      console.log('[@review/events] Connected successfully to server');
+      init = false;
+    }
+    const db = client.db(dbName);
+    callback(db, () => client.close());
+  });
+}
+
+export default doMongo;
