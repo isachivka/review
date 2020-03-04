a1d98a43326dcdb40f5f3a28c1a833b2c2662a51
isachivka <juicy.igor@gmail.com>
2020-03-04 22:11:33 +0300

    [@review/events] Rewrite logs by new logger

--- events/src/events/sample.ts
+++ events/src/events/sample.ts
@@ -1,9 +1,10 @@
 import { PullRequests } from '@review/github-fetch/src/types/shared';
 import { Generator, GeneratorResult } from '../generateEvents';
+import logs from '@review/logs';
 
 const sampleEventGenerator: Generator =
   async (prev: PullRequests, next: PullRequests): Promise<GeneratorResult> => {
-    console.log(prev, next);
+    logs.events.log('Sample generator: ', prev, next);
     return [];
   };
 
