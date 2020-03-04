27ee617710ae2c04513d1cfedf56b3e092bbc8e0
isachivka <juicy.igor@gmail.com>
2020-03-04 21:54:26 +0300

    [@review/logs] Add @review/logs module

---
+++ logs/index.ts
@@ -0,0 +1,48 @@
+const modulesLocale = {
+  events: '[@review/events]',
+  gitHubFetch: '[@review/github-fetch]',
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type consoleLog = (mess: any, ...opt: any[]) => void
+
+interface Logger {
+  log: consoleLog;
+  warn: consoleLog;
+  error: consoleLog;
+}
+
+class Console implements Logger {
+  _module: string;
+
+  constructor(module: string) {
+    this._module = module;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  log(...args: any): void {
+    console.log(this._module, ...args);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  warn(...args: any): void {
+    console.warn(this._module, ...args)
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  error(...args: any): void {
+    console.error(this._module, ...args)
+  }
+}
+
+class Log {
+  events: Logger;
+  githubFetch: Logger;
+
+  constructor() {
+    this.events = new Console(modulesLocale.events);
+    this.githubFetch = new Console(modulesLocale.gitHubFetch);
+  }
+}
+
+export default new Log();
