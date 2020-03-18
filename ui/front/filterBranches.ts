2d43b20cf9ab9eb19aec455cfe602859ce3e1bc2
isachivka <juicy.igor@gmail.com>
2020-03-18 23:01:30 +0300

    [@review/ui] Lint

--- ui/front/filterBranches.ts
+++ ui/front/filterBranches.ts
@@ -3,8 +3,8 @@ import { MappedBranches, MappedBranch } from '@review/github-fetch/src/branches'
 const reserved = /^develop|master|release\/\d+(\.\d+){1,2}$/;
 const valid = /^(feature|bugfix)(\/\d+(\.\d+){1,2})?\/(JSF|SNF)[^/]+$/;
 
-const isValid = (branch: { branch: string }) => valid.test(branch.branch);
-const isntReserved = (branch: { branch: string }) => !reserved.test(branch.branch);
+const isValid = (branch: { branch: string }): boolean => valid.test(branch.branch);
+const isntReserved = (branch: { branch: string }): boolean => !reserved.test(branch.branch);
 
 function mapBranches(branches: MappedBranches): (MappedBranch & { invalid?: boolean })[] {
   return branches
