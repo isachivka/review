65e0d66df2793080eabd127302a5062418339526
Igor Sachivka <juicy.igor@gmail.com>
2020-07-21 18:56:21 +0300

    [@review/events] Add put API for merged pull requests

--- events/src/index.ts
+++ events/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import { PullRequests } from '@review/github-fetch/src/types/shared';
 import doMongo from '@review/core/doMongo';
 import generateEvents from './generateEvents';
@@ -7,46 +8,59 @@ import logs from '@review/logs';
 import pullRequestCreated from './events/pullRequestCreated';
 import pullRequestApproved from './events/pullRequestApproved';
 
-const generators = [
-  pullRequestCreated,
-  pullRequestApproved,
-];
-
 const port = 4000;
 const app = express();
-app.use(express.json());
+app.use(bodyParser.json({ limit: '10mb' }));
 
-app.put('/put', (request, response) => {
-  const pullRequests: PullRequests = request.body;
-  response.status(200).send('Okay');
+const putPulls = [
+  {
+    url: '/putOpened',
+    collection: 'prs',
+    generators: [
+      pullRequestCreated,
+      pullRequestApproved,
+    ]
+  },
+  {
+    url: '/putMerged',
+    collection: 'mergedPrs',
+    generators: [],
+  },
+];
 
-  doMongo(async (db, close) => {
-    try {
-      const pullRequestsCollection = db.collection('prs');
-      const prevPullRequests = await pullRequestsCollection.find({}).toArray();
-      await pullRequestsCollection.deleteMany({});
-      await pullRequestsCollection.insertMany(pullRequests);
+putPulls.forEach(({ url, collection, generators }) => {
+  app.put(url, (request, response) => {
+    const pullRequests: PullRequests = request.body;
+    response.status(200).send('Okay');
 
-      // Generate event if we have data for compare
-      // and save it to mongodb
-      if (prevPullRequests.length > 0) {
-        const events = await generateEvents(
-          generators,
-          prevPullRequests,
-          pullRequests,
-        );
+    doMongo(async (db, close) => {
+      try {
+        const pullRequestsCollection = db.collection(collection);
+        const prevPullRequests = await pullRequestsCollection.find({}).toArray();
+        await pullRequestsCollection.deleteMany({});
+        await pullRequestsCollection.insertMany(pullRequests);
 
-        const eventsCollection = db.collection('events');
-        if (events.length > 0) {
-          await eventsCollection.insertMany(events);
-          logs.events.log('Events written to collection:', events);
+        // Generate event if we have data for compare
+        // and save it to mongodb
+        if (prevPullRequests.length > 0) {
+          const events = await generateEvents(
+            generators,
+            prevPullRequests,
+            pullRequests,
+          );
+
+          const eventsCollection = db.collection('events');
+          if (events.length > 0) {
+            await eventsCollection.insertMany(events);
+            logs.events.log('Events written to collection:', events);
+          }
         }
+      } catch (e) {
+        logs.events.error(e);
       }
-    } catch (e) {
-      logs.events.error(e);
-    }
 
-    close();
+      close();
+    });
   });
 });
 
