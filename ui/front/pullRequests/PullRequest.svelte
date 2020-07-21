4da4c32db835ed0416f0f51025c660f1c85adaeb
Igor Sachivka <juicy.igor@gmail.com>
2020-07-21 19:07:14 +0300

    [@review/ui] Add new page for MergedPullRequests

--- ui/front/pullRequests/PullRequest.svelte
+++ ui/front/pullRequests/PullRequest.svelte
@@ -4,6 +4,7 @@
   import last from 'lodash/last';
   import groupBy from 'lodash/groupBy';
   import Badge from './Badge.svelte';
+  import FromTo from './FromTo.svelte';
 
   export let pullRequest;
 
@@ -17,7 +18,7 @@
   $: ciStatus = pullRequest.commits.nodes[0].commit.status.state;
   $: prNumber = last(pullRequest.permalink.split('/'));
   $: age = parseInt(
-    (Date.now() - new Date(pullRequest.createdAt)) / 1000 / 60 / 60 / 24
+    (Date.now() - new Date(pullRequest.createdAt)) / 1000 / 60 / 60 / 24,
   );
 </script>
 
@@ -29,11 +30,7 @@
     <a href={pullRequest.permalink} target="_blank">#{prNumber}</a>
   </td>
   <td class="second">{age} days</td>
-  <td class="fixWidth">
-    <span class="from">{pullRequest.headRefName}</span>
-    <span class="to">{pullRequest.baseRefName}</span>
-    <span class="icon" />
-  </td>
+  <td><FromTo pullRequest={pullRequest} /></td>
   <td><Badge value={reviews.APPROVED} type="approve" /></td>
   <td><Badge value={reviews.CHANGES_REQUESTED} type="change" /></td>
   <td><Badge value={ciStatus} type="ci" /></td>
@@ -48,37 +45,5 @@
   }
   .second {
     padding: 0 10px;
-  }
-  .fixWidth {
-    width: 300px;
-    white-space: nowrap;
-    text-overflow: ellipsis;
-    overflow: hidden;
-    position: relative;
-    height: 40px;
-    font-size: 12px;
-  }
-
-  .fixWidth .to {
-    position: absolute;
-    top: 19px;
-    left: calc(33px + 10px);
-    z-index: 5;
-  }
-  .fixWidth .from {
-    position: absolute;
-    top: 0;
-    left: calc(15px + 10px);
-    z-index: 5;
-  }
-  .fixWidth .icon {
-    position: absolute;
-    top: 4px;
-    left: calc(0 + 10px);
-    width: 28px;
-    height: 28px;
-    background: linear-gradient(90deg, #b425c5, #b90e45);
-    -webkit-mask: url('/images/branch.svg') no-repeat center;
-    -webkit-mask-size: 28px 28px;
   }
 </style>
--- ui/front/store.ts
+++ ui/front/store.ts
@@ -3,6 +3,7 @@ import { PullRequests, Branches } from '@review/github-fetch/src/types/shared';
 import { AnyEvent } from '@review/events/src/events/types';
 
 export const pullRequests = writable<PullRequests>([]);
+export const mergedPullRequests = writable<PullRequests>([]);
 export const branches = writable<Branches>([]);
 export const events = writable<AnyEvent[]>([]);
 export const stacktrace = writable([]);
