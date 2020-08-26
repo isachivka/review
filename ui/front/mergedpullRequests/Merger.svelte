a3ca07839d117f5050d65873900ef8947ade5679
Igor Sachivka <juicy.igor@gmail.com>
2020-08-26 17:44:59 +0300

    [@review/ui] Rework overview page

--- ui/front/mergedpullRequests/Merger.svelte
+++ ui/front/mergedpullRequests/Merger.svelte
@@ -1,31 +1,31 @@
 <script>
-  export let merger;
+  export let person;
   export let total;
 
   import { link } from 'svelte-routing';
 
-  const login = merger[0].mergedBy.login;
-  const avatarUrl = merger[0].mergedBy.avatarUrl;
-  const style = `width: ${merger.length / total * 100}%`;
-
   function onMouseDown() {
-    localStorage.setItem('searchMergedRegExpString', login);
+    localStorage.setItem('searchMergedRegExpString', person.user.login);
   }
 </script>
 
 <div class="merger">
   <div class="left">
-    <img src={avatarUrl} alt={login}>
+    <img src={person.user.avatarUrl}>
   </div>
   <div class="right">
-    <div>{login} merged</div>
+    <div>{person.user.login} </div>
     <span class="total">
-      <span class="user" style={style} />
+      <span class="user" style={`width: ${person.mergedBy.length / total * 100}%`} />
+      <span class="user c1" style={`width: ${person.createdBy.length / total * 100}%`} />
+      <span class="user c2" style={`width: ${person.reviewedBy.length / total * 100}%`} />
     </span>
     <div>
       <a on:mousedown={onMouseDown} use:link href="/merged/timeline">
-        {merger.length} pull request
+        merge {person.mergedBy.length} pull requests
       </a>
+      <div>create {person.createdBy.length} pull requests</div>
+      <div>review {person.reviewedBy.length} pull requests</div>
     </div>
   </div>
 </div>
@@ -40,6 +40,8 @@
     width: 60px;
     height: 60px;
     border-radius: 60px;
+    overflow: hidden;
+    display: block;
   }
   .left {
     width: 80px;
@@ -52,16 +54,21 @@
     margin: 4px 0;
     display: block;
     width: 100%;
-    height: 6px;
+    height: 32px;
     border-radius: 3px;
   }
 
   span.user {
     height: 6px;
     display: block;
-    background: #b425c5;
+    background: #25c560;
     border-radius: 3px;
+    margin-bottom: 5px;
+    min-width: 7px;
   }
+
+  span.user.c1 { background: #d4cc23; }
+  span.user.c2 { background: #2585c5; }
 
   a {
     color: #fff;
